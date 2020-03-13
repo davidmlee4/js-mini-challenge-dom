b94be10ea17c522f58c1a6efb2f63f5ac4402b61
davidmlee4 <davidmlee4@gmail.com>
2020-03-12 21:39:21 -0400

    more DRY

--- index.js
+++ index.js
@@ -8,29 +8,49 @@ console.log(header)
 /***** Deliverable 2 *****/
 header.style.color = "red"
 
-PLAYERS.forEach(player=> addPlayer(player))
+
+
+
 
 /***** Deliverable 3 *****/
 // console.log("PLAYERS array looks like this:", PLAYERS)
 
-function addPlayer(player){
-    let playerDiv = document.createElement("div")
-    let playerH3 = document.createElement("h3")
-    let playerImage = document.createElement("img")
+
+PLAYERS.forEach(player=> {
+let p = document.createElement("p")
+p.innerHTML = `
+<div class="player" data-number="${player.number}">
+  <h3>
+    ${player.name} (<em>${player.nickname}</em>)
+  </h3>
+  <img src="${player.photo}" alt="${player.name}">
+</div>
+`
+playerContainer.append(p)
+
+})
 
 
-    playerDiv.className = "player"
-    playerDiv.dataset.number = `${player.number}`
 
-    playerH3.innerText = `${player.name} (<em>${player.nickname}</em>)`
+// PLAYERS.forEach(player=> addPlayer(player))
+// function addPlayer(player){
+//     let playerDiv = document.createElement("div")
+//     let playerH3 = document.createElement("h3")
+//     let playerImage = document.createElement("img")
 
-    playerImage.src = player.photo
-    playerImage.alt = player.name
 
-    playerContainer.append(playerDiv)
-    playerDiv.append(playerH3)
-    playerDiv.append(playerImage)
-}
+//     playerDiv.className = "player"
+//     playerDiv.dataset.number = `${player.number}`
+
+//     playerH3.innerText = `${player.name} (<em>${player.nickname}</em>)`
+
+//     playerImage.src = player.photo
+//     playerImage.alt = player.name
+
+//     playerContainer.append(playerDiv)
+//     playerDiv.append(playerH3)
+//     playerDiv.append(playerImage)
+// }
 
 /***** Deliverable 4 *****/
 let raheem = document.querySelector(`[data-number='7']`).remove()
